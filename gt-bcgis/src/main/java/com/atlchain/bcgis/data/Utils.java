a7f357d4cf1ac386d5acc7aee0fe4c7d08725a73
stone-ch <great_cy_ang@163.com>
2019-08-13 11:28:28 +0800

    add Utils.java

---
+++ gt-bcgis/src/main/java/com/atlchain/bcgis/data/Utils.java
@@ -0,0 +1,17 @@
+package com.atlchain.bcgis.data;
+
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryCollection;
+import org.locationtech.jts.geom.GeometryFactory;
+
+public class Utils {
+    /**
+     * 构造GeometryCollection对象
+     * @param geomList 空间几何对象列表
+     * @return
+     */
+    public static GeometryCollection getGeometryCollection(Geometry[] geomList) {
+        GeometryFactory geometryFactory = new GeometryFactory();
+        return new GeometryCollection(geomList, geometryFactory);
+    }
+}
