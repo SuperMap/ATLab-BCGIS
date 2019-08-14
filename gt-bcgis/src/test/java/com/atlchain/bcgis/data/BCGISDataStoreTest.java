252d7ccf82e149ed97a9dad3b0ae710157323f8d
stone-ch <great_cy_ang@163.com>
2019-08-14 17:07:10 +0800

    rename BCGISDataStore test class

--- gt-bcgis/src/test/java/com/atlchain/bcgis/data/BCGISDataStoreTest.java
+++ gt-bcgis/src/test/java/com/atlchain/bcgis/data/BCGISDataStoreTest.java
@@ -1,9 +1,13 @@
 package com.atlchain.bcgis.data;
 
 import org.geotools.data.*;
+import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.data.simple.SimpleFeatureStore;
 import org.geotools.data.store.ContentFeatureSource;
+import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.map.FeatureLayer;
 import org.geotools.map.Layer;
@@ -13,17 +17,24 @@ import org.geotools.styling.Style;
 import org.geotools.swing.JMapFrame;
 import org.junit.Assert;
 import org.junit.Test;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.geom.MultiLineString;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.feature.type.GeometryDescriptor;
+import org.opengis.filter.Filter;
+import org.opengis.filter.FilterFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-public class BCGISDataStoreReaderTest {
+public class BCGISDataStoreTest {
 
     private File certFile = new File(this.getClass().getResource("/certs/user/cert.pem").getPath());
     private File keyFile = new File(this.getClass().getResource("/certs/user/user_sk").getPath());
@@ -154,8 +165,8 @@ public class BCGISDataStoreReaderTest {
     // 以JFrame方式显示地图
     public static void main(String[] args) throws IOException {
         BCGISDataStore bcgisDataStore = new BCGISDataStore(
-                new File(BCGISDataStoreReaderTest.class.getResource("/certs/user/cert.pem").getPath()),
-                new File(BCGISDataStoreReaderTest.class.getResource("/certs/user/user_sk").getPath()),
+                new File(BCGISDataStoreTest.class.getResource("/certs/user/cert.pem").getPath()),
+                new File(BCGISDataStoreTest.class.getResource("/certs/user/user_sk").getPath()),
                 "TestOrgA",
                 "grpc://172.16.15.66:7051",
                 "TestOrgA",
@@ -182,4 +193,69 @@ public class BCGISDataStoreReaderTest {
 
        JMapFrame.showMap(map);
    }
+
+    @Test
+    public void testFeatureWrite() throws IOException {
+        String typeName = bcgisDataStore.getTypeNames()[0];
+        SimpleFeatureType  featureType = bcgisDataStore.getSchema(typeName);
+        SimpleFeatureStore featureStore = (SimpleFeatureStore) bcgisDataStore.getFeatureSource(typeName);
+        SimpleFeatureStore featureStore1 = (SimpleFeatureStore) bcgisDataStore.getFeatureSource(typeName);
+        SimpleFeatureStore featureStore2 = (SimpleFeatureStore) bcgisDataStore.getFeatureSource(typeName);
+
+        Transaction t1 = new DefaultTransaction("t1");
+        Transaction t2 = new DefaultTransaction("t2");
+
+        featureStore1.setTransaction(t1);
+        featureStore2.setTransaction(t2);
+
+        System.out.println("Step 1");
+        System.out.println("------");
+        System.out.println(
+                "start     auto-commit: " + DataUtilities.fidSet(featureStore.getFeatures()));
+        System.out.println(
+                "start              t1: " + DataUtilities.fidSet(featureStore1.getFeatures()));
+        System.out.println(
+                "start              t2: " + DataUtilities.fidSet(featureStore2.getFeatures()));
+
+        // 测试删除 featureStore1 中的数据，在事务 t1 commit 之前，删除操作只会记录在 DataStore 中，commit 之后 才会写入数据库
+        FilterFactory ff = CommonFactoryFinder.getFilterFactory(null);
+        Filter filter1 = ff.id(Collections.singleton(ff.featureId("tmpTypeName.1")));
+        featureStore1.removeFeatures(filter1);
+        System.out.println();
+        System.out.println("Step 2 transaction 1 removes feature 'fid1'");
+        System.out.println("------");
+        System.out.println(
+                "t1 remove auto-commit: " + DataUtilities.fidSet(featureStore.getFeatures()));
+        System.out.println(
+                "t1 remove          t1: " + DataUtilities.fidSet(featureStore1.getFeatures()));
+        System.out.println(
+                "t1 remove          t2: " + DataUtilities.fidSet(featureStore2.getFeatures()));
+
+
+        GeometryFactory geometryFactory = new GeometryFactory();
+        LineString lineString = geometryFactory.createLineString(new Coordinate[]{new Coordinate(10.0, 13.0), new Coordinate(23.0, 26.0)});
+        LineString[] lineStrings = {lineString, lineString};
+        MultiLineString multiLineString = geometryFactory.createMultiLineString(lineStrings);
+        SimpleFeature feature = SimpleFeatureBuilder.build(featureType, new Object[]{multiLineString}, "line1");
+        SimpleFeatureCollection collection = DataUtilities.collection(feature);
+        featureStore2.addFeatures(collection);
+
+        System.out.println();
+        System.out.println("Step 3 transaction 2 adds a new feature '" + feature.getID() + "'");
+        System.out.println("------");
+        System.out.println(
+                "t2 add    auto-commit: " + DataUtilities.fidSet(featureStore.getFeatures()));
+        System.out.println(
+                "t2 add             t1: " + DataUtilities.fidSet(featureStore1.getFeatures()));
+        System.out.println(
+                "t1 add             t2: " + DataUtilities.fidSet(featureStore2.getFeatures()));
+
+        // 提交事务
+        t1.commit();
+//        t2.commit();
+
+        t1.close();
+        t2.close();
+        bcgisDataStore.dispose();
+    }
 }
