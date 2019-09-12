9d9ff68fc8569f9541753b9db02dd17678c655be
751232541@qq.com <751232541@qq.com>
2019-09-12 09:58:53 +0800

    结果：实现Jersey框架下的post文件传输

--- src/main/java/com/atlchain/bcgis/storage/FastDFS.java
+++ src/main/java/com/atlchain/bcgis/storage/FastDFS.java
@@ -31,50 +31,18 @@ public class FastDFS {
 
     private final String conf_filename = FastDFS.class.getResource("/fdfs_client.conf").getPath();
 
-    /**
-     * @Get方式：提供资源的只读访问 根据本地资源的路径将其上传到FastDFS数据库，并返回保存的路径信息
-     * @param path
-     * @return
-     */
-    @GET
-    @Path("/saveTofastDFS")
-    @Produces(MediaType.TEXT_PLAIN)
-    public static String put(@QueryParam("path")
-                             @DefaultValue("E:\\DemoRecording\\File_storage\\JerseyTest\\\\test1.jpg") String path) {
-
-        byte[] data = new Utils().fileTobyte(path);
-        String fileExtName = path.substring(path.lastIndexOf('.')+1) ;
-
-        String[] fileIds = new FastDFS().FastDFSUploadFile( data,fileExtName);
-        String GroupID = fileIds[0];
-        String filepath = fileIds[1];
-        return "=====GroupID: " + GroupID + " =====filepath: " + filepath ;
-    }
-
-    @GET
-    @Path("/resourceAddress")
-    @Produces(MediaType.TEXT_PLAIN)
-    public static byte[] putget(@QueryParam("path")
-                                @DefaultValue("E:\\DemoRecording\\File_storage\\JerseyTest\\\\test1.jpg") String path){
-
-        byte[] data = new Utils().fileTobyte(path);
-        String fileExtName = path.substring(path.lastIndexOf('.')+1) ;
-        System.out.println("data" + data.length);
-        return data;
-    }
-
+    // @POST方式：将客户端的文件数据通过字节流的方式上传到FastDFS数据库，并返回保存的路径信息
     @Path("/uploading")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public String uploadImg(
+    public String uploading(
                             @FormDataParam("file") InputStream fileInputStream,
                             @FormDataParam("file") FormDataContentDisposition disposition,
                             @FormDataParam("type") String type,
-                            @FormDataParam("filename") String resourceId ) throws JSONException, IOException {
-
+                            @FormDataParam("filename") String resourceId ) throws JSONException, IOException
+    {
         System.out.println("开始调用");
-
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         byte[] bytes = new byte[1024];
         int n = 0;
@@ -86,27 +54,20 @@ public class FastDFS {
         String[] strs = FastDFSUploadFile(byteArray, "jpg");
         result.put("s0", strs[0]);
         result.put("s1", strs[1]);
-        System.out.println("结束调用！");
+        System.out.println("结束调用！" + result.toString());
         return result.toString();
     }
 
-    /**
-     * @POST方式：用于更新现有资源或者创建一个新资源  读取FastDFS数据库文件并保存到本地设定的路径下
-     * 地址 http://localhost:8080/myapp/storage/fastdfs/head
-     * @param groupID
-     * @param filePath_FastDFS
-     * @param savefilePath_Local
-     * @return
-     */
+    // @POST方式：用于更新现有资源或者创建一个新资源  读取FastDFS数据库文件并保存到本地设定的路径下
     @POST
-    @Path("/downloadTofastDFS")
-    public String ParamPost(@HeaderParam("GroupID")
+    @Path("/download")
+    public String download(@HeaderParam("GroupID")
                             @DefaultValue("group1") String groupID,
                             @HeaderParam("filePath_FastDFS")
                             @DefaultValue("M00/00/00/wKgojV11pm2ELgrMAAAAAGYRQew274.jpg") String filePath_FastDFS,
                             @HeaderParam("savefilePath_Local")
-                            @DefaultValue("E:\\DemoRecording\\File_storage\\JerseyTest\\\\test009.jpg")String savefilePath_Local) {
-
+                            @DefaultValue("E:\\DemoRecording\\File_storage\\JerseyTest\\\\test009.jpg")String savefilePath_Local)
+    {
         List<String> downloadlist = new LinkedList<>();
         downloadlist.add(groupID);
         downloadlist.add(filePath_FastDFS);
@@ -115,23 +76,24 @@ public class FastDFS {
         return "=====GroupID: " + groupID + " =====filepath: " + filePath_FastDFS + "====fileExtName ：" + savefilePath_Local;
     }
 
-    /**
-     * @DELETE 删除目标下的资源 定位FastDFS数据库中的资源，然后删除
-     * @param deleteFileId
-     * @param deleteFilepath
-     */
+    // DELETE 删除目标下的资源 定位FastDFS数据库中的资源，然后删除
     @DELETE
-    @Path("deleteTofastDFS")
-    public void Paramdelete(@QueryParam("deletefileId")
+    @Path("delete")
+    public void delete(@QueryParam("deletefileId")
                             @DefaultValue("group1")String deleteFileId,
                             @QueryParam("deleteFilepath")
-                            @DefaultValue("M00/00/00/wKgojV1x8XiEZ1CTAAAAAGYRQew206.jpg")String deleteFilepath ) {
+                            @DefaultValue("M00/00/00/wKgojV1x8XiEZ1CTAAAAAGYRQew206.jpg")String deleteFilepath )
+    {
 
         List<String> deletelist = new LinkedList<>();
         deletelist.add(deleteFileId);
         deletelist.add(deleteFilepath);
         new FastDFS().FastDFSDeleteFile(deletelist);
     }
+
+    //
+    // <----------------以下代码为实现FastDFS数据库上传、下载和删除的SDK源码---------->
+    //
 
     //  put file to the FastDFS
     private String[] FastDFSUploadFile(byte[] fileContent, String fileExtName){
@@ -164,7 +126,6 @@ public class FastDFS {
         logger.info("upload file sucessful!");
         return fileIds ;
     }
-
 
     //download file from the FastDFS
     private String FastDFSDownloadFile(List<String> args){
