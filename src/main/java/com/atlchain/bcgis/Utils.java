b59b3ae8bc3e713c07f565f6b7060414d624fe76
751232541@qq.com <751232541@qq.com>
2019-09-16 16:17:08 +0800

    增加 MongoDB

--- src/main/java/com/atlchain/bcgis/Utils.java
+++ src/main/java/com/atlchain/bcgis/Utils.java
@@ -1,5 +1,7 @@
 package com.atlchain.bcgis;
 
+import java.security.MessageDigest;
+
 /**
  * 工具类
  */
@@ -11,6 +13,31 @@ public class Utils {
      */
     public static String getExtName(String fileName) {
         int index = fileName.lastIndexOf('.');
-        return fileName.substring(index, fileName.length()-1);
+        return fileName.substring(index, fileName.length());
+    }
+
+    private static String byte2Hex(byte[] bytes) {
+        StringBuffer stringBuffer = new StringBuffer();
+        String temp = null;
+        for (int i = 0; i < bytes.length; i++) {
+            temp = Integer.toHexString(bytes[i] & 0xFF);
+            if (temp.length() == 1) {
+                stringBuffer.append("0");
+            }
+            stringBuffer.append(temp);
+        }
+        return stringBuffer.toString();
+    }
+
+    public static String getSHA256(String str) {
+        if (str == null) {
+            return null;}
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.reset();
+            messageDigest.update(str.getBytes());
+            return byte2Hex(messageDigest.digest());
+        } catch (Exception e) {
+            throw new RuntimeException(e);}
     }
 }
