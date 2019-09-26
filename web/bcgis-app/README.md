c12226114de7cc1527c7d0cbd171bd351e52d4ca
stone-ch <great_cy_ang@163.com>
2019-09-26 16:59:10 +0800

    将 DEMO 前端代码分离到 ATLab-BCGIS-DEMO

--- web/bcgis-app/README.md
+++
@@ -1,3 +0,0 @@
-# BCGIS 系统 DEMO
-
-基于 React 开发的前端应用，主要用于体验 BCGIS 所实现的功能。
--- web/bcgis-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ToolBox from './Toolbox';
-import Publish from './Publish';
-import './index.css';
-
-ReactDOM.render(<ToolBox />, document.getElementById('toolbox'));
-ReactDOM.render(<Publish />, document.getElementById('map'));
